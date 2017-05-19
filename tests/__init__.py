834dd2e56a556ebe308032b9689abccb572a8131
Vasily Sinitsin <vasilysinitsin@protonmail.com>
2017-05-19 20:29:39 +0300

    Tests fixed. Triggering Travis to rebuild.

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,38 +8,35 @@ import faces
 
 class FacesTest(unittest.TestCase):
     def setUp(self):
-        self.valid_image = open('Roosevelt.jpg', 'rb')
-        self.valid_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/President_Roosevelt_-_Pach_Bros.tif/lossy-page1-220px-President_Roosevelt_-_Pach_Bros.tif.jpg'
+        with open('tests/Roosevelt.jpg', 'rb') as valid_image:
+            self.image_by_file = faces.FaceAppImage(file=valid_image)
 
-        self.image_by_file = faces.FaceAppImage(file=self.valid_image)
-        self.image_by_url = faces.FaceAppImage(url=self.valid_url)
+        valid_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/President_Roosevelt_-_Pach_Bros.tif/lossy-page1-220px-President_Roosevelt_-_Pach_Bros.tif.jpg'
+        self.image_by_url = faces.FaceAppImage(url=valid_url)
 
-    def badFileTest(self):
-        bad_file = self.valid_image.read(1)  # Reading just one byte to get invalid image.
-        image = faces.FaceAppImage(file=bad_file)
+    def testBadFile(self):
+        with self.assertRaises(faces.BadImageType):
+            with open('.travis.yml', 'rb') as bad_file:
+                image = faces.FaceAppImage(file=bad_file)
 
-        self.assertRaises(faces.BadImageType)
+    def testNotImage(self):
+        with self.assertRaises(faces.BadImageType):
+            not_image_url = 'https://wikipedia.org'
+            image = faces.FaceAppImage(url=not_image_url)
 
-    def notImageURLTest(self):
-        not_image_url = 'https://wikipedia.org'
-        image = faces.FaceAppImage(url=not_image_url)
+    def testNoFaces(self):
+        with self.assertRaises(faces.ImageHasNoFaces):
+            with open('tests/Enigma.jpg', 'rb') as no_faces_image:
+                image = faces.FaceAppImage(file=no_faces_image)
 
-        self.assertRaises(faces.BadImageType)
-
-    def noFacesTest(self):
-        no_faces_image = open('Enigma.jpg', 'rb')
-        image = faces.FaceAppImage(no_faces_image)
-
-        self.assertRaises(faces.ImageHasNoFaces)
-
-    def filterTest(self):
+    def testFilter(self):
         self.image_by_file.apply_filter('smile')
 
-    def badFilterTest(self):
-        self.image_by_file.apply_filter('Make me as cool as Vasily')
+    def testBadFilter(self):
+        with self.assertRaises(faces.BadFilterID):
+            self.image_by_file.apply_filter('Make_me_as_cool_as_Vasily')
 
-        self.assertRaises(faces.BadFilterID)
 
-
+print(__name__, 'NAME')
 if __name__ == "__main__":
     unittest.main()
